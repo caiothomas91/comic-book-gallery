98228047c2ebbe963199aa708ad73be5a871be8d
Caio Thomas <caiothomas91@gmail.com>
2019-08-29 19:32:44 -0500

    Added design files

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -21,7 +21,7 @@ namespace ComicBookGallery.Controllers
                 "Colors: Edgar Delgado",
                 "Letters: Chris Eliopoulos"
             };
-
+            
             return View();
         }
     }
